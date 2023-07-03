2e9e686c55d410419f543a204d1cace4ce28d15f
PranjaliChouhan <pranjalichouhan4@gmail.com>
2023-07-03 23:37:46 +0530

    initial

---
+++ index.js
@@ -0,0 +1,16 @@
+// Create the first heading element
+var heading1 = document.createElement("h1");
+heading1.textContent = "Heading 1";
+
+// Create the second heading element
+var heading2 = document.createElement("h2");
+heading2.textContent = "Heading 2";
+
+// Create the third heading element
+var heading3 = document.createElement("h3");
+heading3.textContent = "Heading 3";
+
+// Append the headings to the document body
+document.body.appendChild(heading1);
+document.body.appendChild(heading2);
+document.body.appendChild(heading3);
